d4580c2b1be44926670bb5f981a09e323d973ba5
HỮU HƯNG <142713851+hungnguyen26@users.noreply.github.com>
2025-02-17 23:54:45 +0700

    #45

---
+++ src/main/java/com/hungnguyen/laptop_shop/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.hungnguyen.laptop_shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hungnguyen.laptop_shop.service.UserService;
+
+// @Controller
+// public class UserController {
+    
+//     @RequestMapping("/")
+//     public String getHomePage(){
+//         return "hello";
+//     }
+// }
+
+@RestController
+public class UserController {
+    
+    private UserService userService;
+
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping("/")
+    public String getHomePage(){
+        return this.userService.hanldeHello();
+    }
+}
---
+++ src/main/java/com/hungnguyen/laptop_shop/service/UserService.java
@@ -0,0 +1,10 @@
+package com.hungnguyen.laptop_shop.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public String hanldeHello(){
+        return "hello from servicce";
+    }
+}
