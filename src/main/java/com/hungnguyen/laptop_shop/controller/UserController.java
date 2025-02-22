114653c60edd6554c8526a88149f339202ae7b67
HỮU HƯNG <142713851+hungnguyen26@users.noreply.github.com>
2025-02-22 14:13:01 +0700

    #49

--- src/main/java/com/hungnguyen/laptop_shop/controller/UserController.java
+++ src/main/java/com/hungnguyen/laptop_shop/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.hungnguyen.laptop_shop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +18,10 @@ public class UserController {
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
         String test = this.userService.hanldeHello();
+        model.addAttribute("bin", test);
+        model.addAttribute("hungnguyen", "aksdaisdhjas");
         return "hello" ;
     }
 }
