b5f65d06acc5f6269828f5995cb3f7f601c7db44
HỮU HƯNG <142713851+hungnguyen26@users.noreply.github.com>
2025-03-17 22:04:26 +0700

    #83 Design giao diện admin

---
+++ src/main/java/com/hungnguyen/laptop_shop/controller/admin/DashboardController.java
@@ -0,0 +1,13 @@
+package com.hungnguyen.laptop_shop.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping("/admin")
+    public String getDashboard(){
+        return "admin/dashboard/index";
+    }
+}
