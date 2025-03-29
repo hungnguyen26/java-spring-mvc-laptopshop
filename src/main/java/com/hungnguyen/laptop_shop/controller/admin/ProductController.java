a79d0b7d6babbad3d44b1a62f7d1319675443220
HỮU HƯNG <142713851+hungnguyen26@users.noreply.github.com>
2025-03-30 00:06:28 +0700

    Update product

--- src/main/java/com/hungnguyen/laptop_shop/controller/admin/ProductController.java
+++ src/main/java/com/hungnguyen/laptop_shop/controller/admin/ProductController.java
@@ -1,6 +1,7 @@
 package com.hungnguyen.laptop_shop.controller.admin;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,7 +20,7 @@ import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class ProductController {
@@ -33,26 +34,26 @@ public class ProductController {
     }
 
     @GetMapping("/admin/product")
-    public String getProduct(Model model){
+    public String getProduct(Model model) {
         List<Product> products = this.productService.getAllProduct();
         model.addAttribute("products", products);
         return "admin/product/index";
     }
 
     @GetMapping("/admin/product/create")
-    public String getCreateProductPage(Model model){
+    public String getCreateProductPage(Model model) {
         model.addAttribute("newProduct", new Product());
         return "admin/product/create";
     }
 
     @PostMapping("/admin/product/create")
-    public String postCreateProduct(Model model, 
+    public String postCreateProduct(Model model,
             @ModelAttribute("newProduct") @Valid Product pr,
             BindingResult newProductBindingResult,
             @RequestParam("hoidanitFile") MultipartFile file) {
 
         // validdate
-        if(newProductBindingResult.hasErrors()){
+        if (newProductBindingResult.hasErrors()) {
             return "/admin/product/create";
         }
 
@@ -62,7 +63,7 @@ public class ProductController {
         this.productService.createProduct(pr);
         return "redirect:/admin/product";
     }
- 
+
     @GetMapping("/admin/product/{id}")
     public String getdDetailProduct(@PathVariable long id, Model model) {
         Product pr = this.productService.getProductById(id).get();
@@ -73,7 +74,7 @@ public class ProductController {
 
     @GetMapping("/admin/product/delete/{id}")
     public String getDeleteProduct(Model model, @PathVariable long id) {
-         model.addAttribute("id", id);
+        model.addAttribute("id", id);
         model.addAttribute("newProduct", new Product());
         return "admin/product/delete";
     }
@@ -83,5 +84,42 @@ public class ProductController {
         this.productService.deleteProductById(pr.getId());
         return "redirect:/admin/product";
     }
+
+    @GetMapping("/admin/product/update/{id}")
+    public String getUpdateProductPage(Model model, @PathVariable long id) {
+        Optional<Product> currentProduct = this.productService.getProductById(id);
+        model.addAttribute("newProduct", currentProduct.get());
+        return "admin/product/update";
+    }
+
+    @PostMapping("/admin/product/update")
+    public String postUpdateProductPage(@ModelAttribute("newProduct") @Valid Product pr,
+            BindingResult newProductBindingResult,
+            @RequestParam("hoidanitFile") MultipartFile file) {
     
+        if(newProductBindingResult.hasErrors())
+            return "admin/product/update";
+
+        Product currentProduct = this.productService.getProductById(pr.getId()).get();
+        if(currentProduct != null){
+            // update new img
+            if(!file.isEmpty()){
+                String img = this.uploadService.handleSaveUploadFile(file, "product");
+                currentProduct.setImage(img);
+            }
+
+            currentProduct.setName(pr.getName());
+            currentProduct.setPrice(pr.getPrice());
+            currentProduct.setQuantity(pr.getQuantity());
+            currentProduct.setDetailDesc(pr.getDetailDesc());
+            currentProduct.setShortDesc(pr.getShortDesc());
+            currentProduct.setFactory(pr.getFactory());
+            currentProduct.setTarget(pr.getTarget());
+
+            this.productService.createProduct(currentProduct);
+        }
+
+        return "redirect:/admin/product";
+    }
+
 }
