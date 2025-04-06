f8ba24b5a56440573adce2b6bd035707b305971b
HỮU HƯNG <142713851+hungnguyen26@users.noreply.github.com>
2025-04-07 01:17:08 +0700

    LoadUserByUsername

---
+++ src/main/java/com/hungnguyen/laptop_shop/service/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.hungnguyen.laptop_shop.service;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    public CustomUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //logic
+        com.hungnguyen.laptop_shop.domain.User user = this.userService.getUserByEmail(username);
+        if(user == null){
+            throw new UsernameNotFoundException("user not found");
+        }
+
+        return new User(user.getEmail(),user .getPassword(),Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")));
+
+    }
+    
+}
