16fe68ec012b3f97234a95d4d0b50d44b9a08396
HỮU HƯNG <142713851+hungnguyen26@users.noreply.github.com>
2025-04-19 00:23:41 +0700

    cart

--- src/main/java/com/hungnguyen/laptop_shop/domain/User.java
+++ src/main/java/com/hungnguyen/laptop_shop/domain/User.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 import jakarta.validation.constraints.Email;
@@ -55,7 +56,8 @@ public class User {
     @OneToMany(mappedBy = "user")
     private List<Order> orders;
 
-
+    @OneToOne(mappedBy = "user")
+    private Cart cart;
     
     
     public Role getRole() {
